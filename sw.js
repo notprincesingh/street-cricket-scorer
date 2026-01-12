28d6cbee3158ac8e50bbb20fe6dffaf0697ae2be
Prince Singh <notprincesingh@gmail.com>
2026-01-12 18:27:18 +0530

    Updated scoring logic and UI

--- sw.js
+++ sw.js
@@ -1,20 +1,44 @@
-const CACHE = "cricket-scorer-v1";
+const CACHE_NAME = 'korba-gully-test-v1';
+const urlsToCache = [
+  './',
+  './index.html'
+  // If you have other static files (images, css), add them here later
+];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c =>
-      c.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./sw.js"
-      ])
-    )
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
